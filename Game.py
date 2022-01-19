711263c0c48e691030c8dcb45a75aedc4cf0e56b
Warpops <82202642+matisrod@users.noreply.github.com>
2022-01-19 14:49:31 +0100

    Update Game.py
    ''' '''

--- Game.py
+++ Game.py
@@ -5,19 +5,19 @@ class Board:
         self.ligne = 9
         self.taille_case = 3
 
-    def check_col(self,coords):#regarde si y'a un autre chiffre égal sur toute la même colonne
+    def check_col(self,coords): '''regarde si y'a un autre chiffre égal sur toute la même colonne'''
         x, y = coords[0], coords[1]
         for colonne in range(self.colonne):
             if colonne == y:
                 return False
 
-    def check_ligne(self, coords):#regarde si y'a un autre chiffre égal sur toute la même ligne
+    def check_ligne(self, coords): '''regarde si y'a un autre chiffre égal sur toute la même ligne'''
         x, y = coords[0], coords[1]
         for colonne in range(self.ligne):
             if colonne == x:
                 return False
             
-   def get_coords_case(self, coords): #return une liste de tuple de coordonnées qui sont dans la même case que les coordonnées passés en argument
+   def get_coords_case(self, coords): '''return une liste de tuple de coordonnées qui sont dans la même case que les coordonnées passés en argument'''
     x, y = coords[0], coords[1]
     liste_coords_case = []
     x_case = x // self.taille_case
