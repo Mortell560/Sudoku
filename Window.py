f1fe3da53e79b12bb8931e18f70ff61eb7ee2a33
Mortell560 <mortell560@gmail.com>
2022-01-19 16:00:34 +0100

    début de l'affichage

--- Window.py
+++ Window.py
@@ -1,10 +1,21 @@
 import graphics
-
+from Board import Board
 class Window:
     def __init__(self, length: int, width: int) -> None:
-        self.length = length
         self.width = width
+        self.length = length
         graphics.init_fenetre(length, width, 'sudoku')
 
-#tests
-a = Window(500, 500)
+    def ask_move(self):
+        pass
+        
+    def affiche_tableau(self, board: Board):
+        pass
+    
+    def afficher(self, board: list) -> None:
+        graphics.affiche_auto_off()
+        self.affiche_tableau()
+        graphics.affiche_tout()
+        
+###################### Partie Test ##############################
+a = Window(1000, 1000)
