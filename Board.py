d251bacbf77ed2359bb47fc5335dcb1cae4ec1d9
Warpops <82202642+matisrod@users.noreply.github.com>
2022-01-20 10:26:45 +0100

    Update Board.py
    ajout méthode create_board

--- Board.py
+++ Board.py
@@ -4,17 +4,30 @@ class Board:
         self.colonne = 9
         self.ligne = 9
         self.taille_case = 3
+    
+    def create_board(self, n):
+        '''création d'un tableau remplie de 0. Puis on ajoute un nombre n de chiffres aléatoirement dans celui-ci'''
+        nbr_de_zero = self.colonne * self.ligne
+        for zero in range(nbr_de_zero):
+            self.board.append(zero)
 
+        for nombre in range(n):
+            x = random.randint(0,8)
+            y = random.randint(0,8)
+            num_dans_liste = x + y * (self.colonne + 1)
+            nombre_random = random.randint(1,9)
+            if check_cell((x,y), nombre_random):
+                self.board[num_dans_liste] = nombre_random
+    
     def check_colonne(self,coords, value): 
         """regarde si y'a un autre chiffre égal sur toute la même colonne"""
         x, y = coords[0], coords[1]
-        value_x_y = self.get_value(coords)
         for colonne in range(self.colonne):
             if colonne == y:
                 continue
             else:
                 value_coords_act = self.get_value(x, colonne)
-                if value_coords_act == value_x_y:
+                if value_coords_act == value:
                     return False
         return True
                 
@@ -22,13 +35,12 @@ class Board:
     def check_ligne(self, coords, value): 
         """regarde si y'a un autre chiffre égal sur toute la même ligne"""
         x, y = coords[0], coords[1]
-        value_x_y = self.get_value(coords)
         for ligne in range(self.ligne):
             if ligne == x:
                 continue
             else:
                 value_coords_act = self.get_value(ligne, y)
-                if value_coords_act == value_x_y:
+                if value_coords_act == value:
                     return False
         return True
             
