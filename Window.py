19fbf7bbce3fc1ec1b94b49d5b5a3d863b5c222a
Mortell560 <mortell560@gmail.com>
2022-01-21 15:54:22 +0100

    temp

--- Window.py
+++ Window.py
@@ -6,9 +6,15 @@ class Window:
         self.length = length
         graphics.init_fenetre(length, width, 'sudoku')
 
+    def show_menu(self):
+        graphics.affiche_texte("Sudoku", (self.width - 100, self.length // 2 - 50), taille=50, couleur=graphics.noir)
+
     def ask_move(self):
+        """Waits for a click and returns the coordinates of the click"""
+        coords = graphics.wait_clic()
         pass
-        
+
+
     def affiche_tableau(self, board: Board):
         pass
     
@@ -19,3 +25,4 @@ class Window:
         
 ###################### Partie Test ##############################
 a = Window(1000, 1000)
+a.show_menu()
