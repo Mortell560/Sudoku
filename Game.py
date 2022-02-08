6867d5bdeeae714524db19225c4b034fabe878e8
Yamaaaaan <98805849+Yamaaaaan@users.noreply.github.com>
2022-02-08 09:42:18 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,3 +6,7 @@ class Game:
         val=self.B[coords]
         self.B[coords]=num
         self.last_moves.empiler((coords,val))
+
+    def retour(self):
+        p=self.last_move.depiler()
+        self.B[p[0]]=p[1]
