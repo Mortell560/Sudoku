9ba917c32282406bd56786c5c9526dd981652314
Warpops <82202642+matisrod@users.noreply.github.com>
2022-02-09 11:02:30 +0100

    again

--- Game.py
+++ Game.py
@@ -1,4 +1,25 @@
 class Game:
+    def run(self):
+        while self.graphics.pas_echap():
+            self.W.afficher(self.B)
+            action = self.W.ask_action() #tuple(tuple(x,y), Action) | None | tuple(None, Action)
+            if action[1] == self.W.PLACE:
+                value = self.W.ask_value()
+                self.place(action[0], value)
+
+            elif action[1] == self.W.RETURN:
+                if self.last_moves.est_vide():
+                    continue
+                else:
+                    self.retour()
+
+            elif action[1] == self.W.RESET:
+                self.B.create_board(self.n) # ne pas oublié le n nombre de case mise dans le jeu
+                while not self.last_moves.est_vide():
+                    self.last_moves.depiler()
+
+            elif action[1] == self.W.SOLVE:
+                self.B.solve()
     
     def place(self,coords,num):
         val=self.B[coords]
